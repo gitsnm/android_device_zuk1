aca4f337d37d3a40d11fa2e6349315f4dec61fcf
ShoaiibGCP <shoaib.gcp@gmail.com>
2018-02-22 17:15:17 +0000

    Revert "Revert "Add oppo power taken from omni""
    This reverts commit cc1dc2a4d6d319ac0b1ac982509ade422a313149.

--- power/util.h
+++ power/util.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2016 The CyanogenMod Project
+ * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2014 The OmniROM Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,18 +14,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#ifndef POWER_UTIL_H
+#define POWER_UTIL_H
 
-#define LOG_TAG "PowerHAL_MSM8974_Ext"
+void sysfs_write(const char *path, const char *s);
+int sysfs_write_silent(const char *path, const char *s);
+int sysfs_read(const char *path, char *s, int num_bytes);
+int sysfs_read_buf(const char* path, char *buf, int size);
+int get_max_freq(char *freq, int size);
+int get_max_cpus(char *cpus, int size);
+void write_cpufreq_value(int cpu, const char* key, const char* value);
 
-#include <utils/Log.h>
-#include "utils.h"
-
-#define BIG_MIN_CPU_PATH "/sys/devices/system/cpu/cpu4/core_ctl/min_cpus"
-#define BIG_MAX_CPU_PATH "/sys/devices/system/cpu/cpu4/core_ctl/max_cpus"
-
-void cm_power_set_interactive_ext(int on)
-{
-    ALOGD("%sabling big CPU cluster", on ? "En" : "Dis");
-    sysfs_write(BIG_MAX_CPU_PATH, on ? "4" : "0");
-    sysfs_write(BIG_MIN_CPU_PATH, on ? "1" : "0");
-}
+#endif
